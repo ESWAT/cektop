3e8c65bdee6692345bda47776fe12a10f4de945e
Philip Karpiak <operations@eswat.ca>
2015-04-10 17:08:08 -0400

    Switch most tasks to Gulp, remove CoffeeScript

---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+var paths = {
+  assets  : 'src/assets/**/*',
+  images  : 'src/images/**/*.{png,jpg,gif}',
+  jade    : 'src/**/*.jade',
+  scripts : 'src/script/**/*.js',
+  stylus  : 'src/stylus/**/*.styl',
+  release : 'release/'
+};
+
+var locals = {
+  title       : pkg.name,
+  author      : pkg.author,
+  description : pkg.description,
+  version     : pkg.version
+}
+
+gulp.task('clean', function(cb) {
+  del(paths.release, cb);
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['js:dev']);
+  gulp.watch(paths.jade, ['jade:dev']);
+  gulp.watch(paths.stylus, ['stylus:dev']);
+  gulp.watch(paths.images, ['images:dev']);
+  gulp.watch(paths.assets, ['assets']);
+});
+
+gulp.task('assets', function() {
+  gulp.src(paths.assets)
+    .pipe(gulp.dest(paths.release + 'assets/'));
+});
+
+gulp.task('connect:dev', function() {
+  connect.server({
+    root: paths.release,
+    port: 8000,
+    livereload: true
+  });
+});
+
+gulp.task('connect:rel', function() {
+  connect.server({
+    root: paths.release,
+    port: 8000
+  });
+});
+
+gulp.task('imagemin:dev', function() {
+  gulp.src(paths.images)
+    .pipe(gulp.dest(paths.release + 'images/'))
+});
+
+gulp.task('imagemin:rel', function() {
+  gulp.src(paths.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.release + 'images/'))
+});
+
+gulp.task('js:dev', function() {
+  gulp.src(paths.scripts)
+    .pipe(gulp.dest(paths.release + 'js/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js:rel', function() {
+  gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.release + 'js/'))
+});
+
+gulp.task('jade:dev', function() {
+  gulp.src(paths.jade)
+    .pipe(jade({
+      pretty: true,
+      locals: locals
+    }))
+    .pipe(gulp.dest(paths.release))
+    .pipe(connect.reload());
+});
+
+gulp.task('jade:rel', function() {
+  gulp.src(paths.jade)
+    .pipe(jade())
+    .pipe(gulp.dest(paths.release))
+});
+
+gulp.task('stylus:dev', function() {
+  gulp.src(paths.stylus)
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+      lineos: true
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.release + 'css/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('stylus:rel', function() {
+  gulp.src(paths.stylus)
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(gulp.dest(paths.release + 'css/'))
+});
+
+// Start server in development mode
+gulp.task('default', ['clean'], function() {
+  gulp.start(
+    'jade:dev',
+    'stylus:dev',
+    'js:dev',
+    'assets',
+    'imagemin:dev',
+    'connect:dev',
+    'watch'
+  );
+});
+
+// Start server in preview mode
+gulp.task('preview', ['clean'], function() {
+  gulp.start(
+    'clean',
+    'jade:rel',
+    'stylus:rel',
+    'js:rel',
+    'assets',
+    'imagemin:rel',
+    'connect:rel'
+  );
+});
+
+// Build optimized files
+gulp.task('build', ['clean'], function() {
+  gulp.start(
+    'clean',
+    'jade:rel',
+    'stylus:rel',
+    'js:rel',
+    'assets',
+    'imagemin:rel'
+  );
+});
